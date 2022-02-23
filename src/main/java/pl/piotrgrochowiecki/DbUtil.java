e60395dbb2304bcd50894ac76075736fa9a312e6
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2022-02-23 18:59:43 +0100

    "connect" method added to DBUtil class

--- src/main/java/pl/piotrgrochowiecki/DbUtil.java
+++ src/main/java/pl/piotrgrochowiecki/DbUtil.java
@@ -1,9 +1,17 @@
 package pl.piotrgrochowiecki;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbUtil {
 
     public static final String SERVER_URL = "jdbc:mysql://localhost:3306";
     public static final String USER = "root";
     public static final String PASSWORD = "coderslab";
 
+    public static Connection connect() throws SQLException {
+        Connection connection = DriverManager.getConnection(SERVER_URL, USER, PASSWORD);
+        return connection;
+    }
 }
