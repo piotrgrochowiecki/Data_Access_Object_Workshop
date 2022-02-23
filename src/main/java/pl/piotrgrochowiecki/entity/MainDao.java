70fc8419a530c532af720492044d803d556ae05d
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2022-02-23 21:01:04 +0100

    Test of "create" method

--- src/main/java/pl/piotrgrochowiecki/entity/MainDao.java
+++ src/main/java/pl/piotrgrochowiecki/entity/MainDao.java
@@ -2,6 +2,12 @@ package pl.piotrgrochowiecki.entity;
 
 public class MainDao {
     public static void main(String[] args) {
+        User testUser1 = new User();
+        testUser1.setUserName("robertos32904");
+        testUser1.setEmail("robert121@wp.pl");
+        testUser1.setPassword("pies123");
 
+        UserDao userDao1 = new UserDao();
+        userDao1.create(testUser1);
     }
 }
