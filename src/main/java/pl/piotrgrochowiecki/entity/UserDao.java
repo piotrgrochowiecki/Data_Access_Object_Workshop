cf884623d64e7422160fef149f497e3b249b2fe7
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2022-02-23 20:56:05 +0100

    methods "create" and "hashPassword" added to "UserDao" class

--- src/main/java/pl/piotrgrochowiecki/entity/UserDao.java
+++ src/main/java/pl/piotrgrochowiecki/entity/UserDao.java
@@ -1,5 +1,10 @@
 package pl.piotrgrochowiecki.entity;
 
+import org.mindrot.jbcrypt.BCrypt;
+import pl.piotrgrochowiecki.DbUtil;
+
+import java.sql.*;
+
 public class UserDao {
 
     private static final String CREATE_USER_QUERY = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
@@ -7,4 +12,27 @@ public class UserDao {
     private static final String UPDATE_USER_BY_ID_QUERY = "UPDATE users SET email = ?, username = ? WHERE id = 1";
     private static final String DELETE_USER_BY_ID_QUERY = "DELETE FROM users WHERE id = ?";
 
+    public User create(User user) {
+        try (Connection conn = DbUtil.connect("workshop2")) {
+            PreparedStatement statement =
+                    conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, hashPassword(user.getPassword()));
+            statement.executeUpdate();
+            //Pobieramy wstawiony do bazy identyfikator, a następnie ustawiamy id obiektu user.
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
 }
