b2d872ffb022fa97484eb833f13e94727221d54a
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2022-02-23 18:55:03 +0100

    Database connection info added to DBUtil class

---
+++ src/main/java/pl/piotrgrochowiecki/DbUtil.java
@@ -0,0 +1,9 @@
+package pl.piotrgrochowiecki;
+
+public class DbUtil {
+
+    public static final String SERVER_URL = "jdbc:mysql://localhost:3306";
+    public static final String USER = "root";
+    public static final String PASSWORD = "coderslab";
+
+}
