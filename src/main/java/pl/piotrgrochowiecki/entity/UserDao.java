fc57812168bdfec5d47b4f03d552619da509e5e0
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2022-02-27 22:56:13 +0100

    added final string with database name

--- src/main/java/pl/piotrgrochowiecki/entity/UserDao.java
+++ src/main/java/pl/piotrgrochowiecki/entity/UserDao.java
@@ -7,13 +7,15 @@ import java.sql.*;
 
 public class UserDao {
 
+    private static final String DATABASE = "workshop2";
+
     private static final String CREATE_USER_QUERY = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
     private static final String READ_USER_BY_ID_QUERY = "SELECT * FROM users WHERE id = ?";
     private static final String UPDATE_USER_BY_ID_QUERY = "UPDATE users SET username = ?, email = ? WHERE id = ?";
     private static final String DELETE_USER_BY_ID_QUERY = "DELETE FROM users WHERE id = ?";
 
     public User create(User user) {
-        try (Connection conn = DbUtil.connect("workshop2")) {
+        try (Connection conn = DbUtil.connect(DATABASE)) {
             PreparedStatement statement =
                     conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
             statement.setString(1, user.getUserName());
@@ -37,7 +39,7 @@ public class UserDao {
     }
 
     public User read(int userId) {
-        try (Connection conn = DbUtil.connect("workshop2")) {
+        try (Connection conn = DbUtil.connect(DATABASE)) {
             PreparedStatement statement = conn.prepareStatement(READ_USER_BY_ID_QUERY);
             statement.setInt(1, userId);
             ResultSet resultSet = statement.executeQuery();
@@ -56,7 +58,7 @@ public class UserDao {
     }
 
     public void update(User user){
-        try (Connection conn = DbUtil.connect("workshop2")) {
+        try (Connection conn = DbUtil.connect(DATABASE)) {
             PreparedStatement statement = conn.prepareStatement(UPDATE_USER_BY_ID_QUERY);
             statement.setString(1, user.getUserName());
             statement.setString(2, user.getEmail());
@@ -68,7 +70,7 @@ public class UserDao {
     }
 
     public void delete(int id) {
-        try (Connection conn = DbUtil.connect("workshop2")) {
+        try (Connection conn = DbUtil.connect(DATABASE)) {
             PreparedStatement statement = conn.prepareStatement(DELETE_USER_BY_ID_QUERY);
             statement.setInt(1, id);
             statement.executeUpdate();
